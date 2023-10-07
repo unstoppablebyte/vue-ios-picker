3360d1bc28442ef3576e8f2d00f8b6d2e6c2bf17
Adrijan Mitrov <unstoppablebyte@gmail.com>
2023-10-07 17:29:53 +0200

    add mobule build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,35 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import {resolve} from 'path'
+import {defineConfig} from 'vite'
+import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
+    plugins: [vue()],
+    build: {
+        lib: {
+            // Could also be a dictionary or array of multiple entry points
+            entry: resolve(__dirname, 'src/index.js'),
+            name: 'IosPicker',
+            // the proper extensions will be added
+            fileName: 'ios-picker',
+            formats: ['es', 'cjs', 'umd'], // Include 'umd' format
+        },
+        rollupOptions: {
+            // make sure to externalize deps that shouldn't be bundled
+            // into your library
+            external: ['vue'],
+            output: {
+                format: ['cjs', 'es', 'umd'], // Include 'umd' format
+                // Provide global variables to use in the UMD build
+                // for externalized deps
+                globals: {
+                    vue: 'Vue',
+                },
+            },
+        },
+    },
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src'),
+        }
     }
-  },
 })
